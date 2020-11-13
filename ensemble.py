50759d08b1f8af583e300c8963567ada7a16eff1
Jaeheon <jaeheon.dev@gmail.com>
2020-11-13 12:42:12 +0900

    Update ensemble.py

--- ensemble.py
+++ ensemble.py
@@ -1,19 +1,29 @@
+import os
+import time
 import timm
 import random
-import numpy as np
-import torch
 import argparse
+import numpy as np
+from tqdm import tqdm, trange
+
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader, random_split
+
 from efficientnet_pytorch import EfficientNet
 from cutmix.cutmix import CutMix
 from cutmix.utils import CutMixCrossEntropyLoss
+from dataset import Dacon
 
 '''
 main에서 돌리지 말고
 그냥 이 파일 자체롤 앙상블을 돌리는게 나을 듯.
 '''
 
+#########setting hyperparameters in here########
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print('use: ',device)
 
@@ -24,86 +34,129 @@ torch.backends.cudnn.benchmark = False
 np.random.seed(777)
 random.seed(777)
 
+DIR = os.path.join(os.getcwd(),'..', 'data') + '/'
 LEARNING_RATE = 1e-3
 LR_STEP = 3
 LR_FACTOR = 0.5
+NUM_WORKERS = 4
 NUM_CLASSES = 1049
 
+TRAIN_PARAMETERS = {
+    'mean':[0.4461, 0.4447, 0.4490],
+    'std':[0.2598, 0.2591, 0.2590]
+}
+
+################################################
 
 class Ensemble_1(nn.Module):
     '''
     이건 cat으로 병합
     '''
-    def __init__(self, model_A, model_B, NUM_CLASSES):
+    def __init__(self, model_A, model_B, model_C, model_D, NUM_CLASSES):
         super(Ensemble_1, self).__init__()
         self.model_A = model_A
         self.model_B = model_B
+        self.model_C = model_C
+        self.model_D = model_D
         #self.model_C = model_C
 
         #Remove Last Linear Layer
         self.model_A.fc = nn.Identity()
         self.model_B.fc = nn.Identity()
-        #self.model_C.fc = nn.Identity()
+        self.model_C.fc = nn.Identity()
+        self.model_D.fc = nn.Identity()
 
         #Create new classifier
-        self.classifier = nn.Linear(2048, NUM_CLASSES)
+        self.classifier = nn.Linear(5195, NUM_CLASSES)
 
     def forward(self, x):
-        x1 = self.model_A(x.clone())
-        x1 = x1.view(x1.size(0), -1)
-        x2 = self.model_B(x.clone())
-        x2 = x2.view(x2.size(0), -1)
-        output = torch.cat((x1, x2), dim=1)
+        output_1 = self.model_A(x)
+        output_1 = output_1.view(output_1.size(0), -1)
+        output_2 = self.model_B(x)
+        output_2 = output_2.view(output_2.size(0), -1)
+        output_3 = self.model_C(x)
+        output_3 = output_3.view(output_3.size(0), -1)
+        output_4 = self.model_D(x)
+        output_4 = output_4.view(output_4.size(0), -1)
 
-        return self.classifier(F.relu(output))
+        output = torch.cat((output_1, output_2, output_3, output_4), dim=1)
+        #import pdb;pdb.set_trace()
+        return self.classifier(output)
 
 class Ensemble_2(nn.Module):
     '''
     이건 그냥 아웃풋 더하기
     '''
-    def __init__(self, model_A, model_B, NUM_CLASSES):
+    def __init__(self, model_A, model_B, model_C, model_D, NUM_CLASSES):
         super(Ensemble_2, self).__init__()
         self.model_A = model_A
         self.model_B = model_B
+        self.model_C = model_C
+        self.model_D = model_D
 
-        self.classifier = nn.Linear(2048, NUM_CLASSES)
     def forward(self, x):
         output_1 = self.model_A(x)
         output_2 = self.model_B(x)
-        output = output_1 + output_2 
+        output_3 = self.model_C(x)
+        output_4 = self.model_D(x)
+        output = output_1 + output_2 + output_3 +output_4
 
-        return self.classifier(F.relu(output))
+        return output
+
+def ck_load(path, model):
+    ckpoint = torch.load(path)
+    model.load_state_dict(ckpoint['model_state_dict'])
+    for param in model.parameters():
+        param.requires_grad=False
+    
+    return model
+
+def submission(test_loader, model):
+    model.eval()
+    landmark_id = []
+    conf = []
+    for images in tqdm(testloader):
+        pred = model(images.to(device))
+        pred = nn.Softmax(dim=1)(pred)
+        pred = pred.detach().cpu()
+        landmark_id.append(torch.argmax(pred, dim=1))
+        conf.append(torch.max(pred, dim=1)[0])
+    submission = test_loader.dataset.submission
+    submission.landmark_id = torch.cat(landmark_id).numpy()
+    submission.conf = torch.cat(conf).numpy()
+    submission.to_csv(os.path.join(os.getcwd(), 'ensemble.csv'), index=False)
 
 
-model_1 = EfficientNet.from_pretrained("efficientnet-b0", num_classes=NUM_CLASSES)        
-model_2 = EfficientNet.from_pretrained("efficientnet-b0", num_classes=NUM_CLASSES)        
+path1 = os.getcwd()+'/b1_cos.pt'
+path2 = os.getcwd()+'/b5.pt'
+path3 = os.getcwd()+'/vit_base_val.pt'
+path4 = os.getcwd()+'/resnext.pt'
 
-optimizer_1 = torch.optim.Adam(model_1.parameters(), lr=LEARNING_RATE)
-optimizer_2 = torch.optim.Adam(model_2.parameters(), lr=LEARNING_RATE)
+model_1 = EfficientNet.from_name("efficientnet-b1", num_classes=NUM_CLASSES)        
+model_2 = EfficientNet.from_name("efficientnet-b5", num_classes=NUM_CLASSES) 
+model_3 = timm.create_model('vit_base_patch16_224', pretrained=False, num_classes=NUM_CLASSES)     
+model_4 = timm.create_model('skresnext50_32x4d', pretrained=False, num_classes=NUM_CLASSES)
 
-lr_scheduler_1 = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer_1, T_0=3)
-lr_scheduler_2 = torch.optim.lr_scheduler.StepLR(optimizer_2, step_size=LR_STEP, gamma=LR_FACTOR)
+print('load checkpoint')
+t1 = time.time()
+model_1 = ck_load(path1, model_1)
+model_2 = ck_load(path2, model_2)
+model_3 = ck_load(path3, model_3)
+model_4 = ck_load(path4, model_4)
+print(f'time:{time.time()-t1:.3f}')
 
-path1 = './b1_cos.pt'
-path2 = './b1_steplr.pt'
+ensemble = Ensemble_2(model_1, model_2, model_3, model_4, NUM_CLASSES)
+ensemble.to(device)
 
-check_epoch = 0
-train_acc_list = []
-valid_acc_list = []
-train_loss_list = []
-valid_loss_list = []
+transforms_train = transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize(TRAIN_PARAMETERS['mean'], TRAIN_PARAMETERS['std'])
+        ])
+
+testset = Dacon(dir=DIR, mode='test', transform=transforms_train)
+testloader = DataLoader(testset, batch_size=64, shuffle=False, num_workers=NUM_WORKERS)
+submission(testloader, ensemble)
 
 
-def inference(path, model, optimizer, lr_scheduler):
-    checkpoint = torch.load(path)
-    check_epoch = checkpoint['epoch']
-    model.load_state_dict(checkpoint['model_state_dict'])
-    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-    lr_scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
-    train_acc_list = checkpoint['train_accuracy']
-    valid_acc_list = checkpoint['val_accuracy']
-    train_loss_list = checkpoint['train_loss']
-    valid_loss_list = checkpoint['val_loss']
-
-    model.to(device)
 
