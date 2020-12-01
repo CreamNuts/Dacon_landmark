f3e2d8534bd9fdcebf0c2a779a4d1dbbe42262ef
creamnuts <hong4497@gmail.com>
2020-12-01 22:41:57 +0900

    Final Update

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import multiprocessing
 import numpy as np
 import torch
 import torch.nn as nn 
-import timm
 import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader, random_split
@@ -15,7 +14,7 @@ from tqdm import tqdm, trange
 from pathlib import Path
 from utils import *
 from dataset import Dacon
-from efficientnet_pytorch import EfficientNet
+
 from cutmix.cutmix import CutMix
 from cutmix.utils import CutMixCrossEntropyLoss
 
@@ -25,23 +24,21 @@ parser.add_argument('--mode', '-m', default='val', choices=['train', 'val', 'tes
 parser.add_argument('--calculator', default='False', help='Cacluate Dataset Mean and Std')
 parser.add_argument('--checkpoint', '-c', default=None, help='Checkpoint Directory')
 parser.add_argument('--save', '-s', default='./Checkpoint.pt', help='Save Directory. if Checkpoint exists, Save Checkpoint in Checkpoint Dir')
-parser.add_argument('--model', default='b1', choices=['b0', 'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'l2', 'vit_base', 'vit_base_hybrid', 'vit_large', 'skresnext50'])
+parser.add_argument('--model', default='b1', choices=['sum', 'weighted_sum', 'b0', 'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'l2', 'vit_base', 'vit_base_hybrid', 'vit_large', 'skresnext50'])
 parser.add_argument('--gpu', default='0')
 parser.add_argument('--cutmix', default=True, help="If True, Use Cutmix Aug in Training")
-parser.add_argument('--scheduler', default='StepLR', choices=['StepLR', 'Cos'])
+parser.add_argument('--scheduler', default='Cos', choices=['StepLR', 'Cos'])
 parser.add_argument('--batchsize', type=int, default=128)
 parser.add_argument('--lr', type=float, default=1e-3)
 parser.add_argument('--epoch', type=int, default=50)
 parser.add_argument('--flooding', type=float, default=0.01)
+parser.add_argument('--num_classes', dest='NUM_CLASSES', type=int, default=1049)
 args = parser.parse_args()
 
 DIR = os.path.join(os.getcwd(),'..', 'data') + '/'
 LR_STEP = 3
 LR_FACTOR = 0.5
-NUM_CLASSES = 1049
 NUM_WORKERS = 4 #multiprocessing.cpu_count() #24
-FLOODING_LEVEL = 0.01
-TRAINING_EPOCH = 10
 
 torch.manual_seed(777)
 torch.cuda.manual_seed(777)
@@ -53,19 +50,10 @@ random.seed(777)
 os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print('use: ',device)
-'''
-parameter 구하는거 매번 하기 귀찮으니까 
-일단 밑에거 긁어서 바로 넣으셈
-이미지 224X224로 새로 계산
-'''
+
 TRAIN_PARAMETERS = {
     'mean':[0.4461, 0.4447, 0.4490],
     'std':[0.2598, 0.2591, 0.2590]
-}
-
-TEST_PARAMETERS = {
-    'mean':[0.4456, 0.4462, 0.4468],
-    'std':[0.2757, 0.2769, 0.2763]
 }
 
 ################################################
@@ -73,49 +61,6 @@ calculation = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor()
 ])
-
-def train(train_loader, model, criterion, optimizer):
-    model.train()
-    acc = 0
-    for images, labels in train_loader:
-        optimizer.zero_grad()
-        output = model(images.to(device))
-        loss = criterion(output, labels.to(device))
-        loss = (loss-args.flooding).abs()+args.flooding
-        loss.backward()
-        optimizer.step()
-        if args.cutmix is True:
-            acc += (output.softmax(dim=1)*labels.to(device)).sum(dim=1).float().mean().detach().cpu()
-        else:
-            acc += (output.argmax(1)==labels.to(device)).float().mean()
-        pbar.set_description("Loss : %.3f" % loss)
-    return acc, loss
-
-def validation(valid_loader, model, criterion):
-    model.eval()
-    acc = 0
-    for images, labels in valid_loader:
-        with torch.no_grad():
-            output = model(images.to(device))
-            loss = criterion(output, labels.to(device))
-            acc += (output.argmax(1)==labels.to(device)).float().mean()
-    return acc, loss
-
-def submission(test_loader, model):
-    model.eval()
-    landmark_id = []
-    conf = []
-    for images in tqdm(testloader):
-        with torch.no_grad():
-            pred = model(images.to(device))
-            pred = nn.Softmax(dim=1)(pred)
-            pred = pred.detach().cpu()
-            landmark_id.append(torch.argmax(pred, dim=1))
-            conf.append(torch.max(pred, dim=1)[0])
-    submission = test_loader.dataset.submission
-    submission.landmark_id = torch.cat(landmark_id).numpy()
-    submission.conf = torch.cat(conf).numpy()
-    submission.to_csv(os.path.join(os.getcwd(), 'submission.csv'), index=False)
 
 if __name__ == '__main__':
     if args.calculator is True:
@@ -134,52 +79,32 @@ if __name__ == '__main__':
             transforms.ToTensor(),
             transforms.Normalize(TRAIN_PARAMETERS['mean'], TRAIN_PARAMETERS['std'])
         ])
-    '''
-    #TensorBoard code
     
-    writer = SummaryWriter('runs/dacon')
-    dacon = Dacon(dir=lab_dir, mode=args.mode, transform=transforms_train)
-    dataloader = DataLoader(dacon, batch_size=args.batchsize, shuffle=True, num_workers=NUM_WORKERS)
-    dataiter = iter(dataloader)
-    images, labels = dataiter.next()
-    img_grid = torchvision.utils.make_grid(images)
-    visualize(img_grid)
-    writer.add_image('dacon_image', img_grid)
-    '''
-    if args.model == 'vit_base':
-        model = timm.create_model('vit_base_patch16_224', pretrained=True, num_classes=NUM_CLASSES)
-    elif args.model == 'vit_base_hybrid':
-        model = timm.create_model('vit_base_resnet26d_224', pretrained=True, num_classes=NUM_CLASSES)
-    elif args.model == 'vit_large':
-        model = timm.create_model('vit_large_patch16_224', pretrained=True, num_classes=NUM_CLASSES)
-    elif args.model == 'skresnext50':
-        model = timm.create_model('skresnext50_32x4d', pretrained=True, num_classes=NUM_CLASSES)
-    else:
-        model = EfficientNet.from_pretrained(f"efficientnet-{args.model}", num_classes=NUM_CLASSES)        
+    model = load_model(args)
     
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
-
-    if args.scheduler == 'StepLR':
-        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=LR_STEP, gamma=LR_FACTOR)
-    elif args.scheduler == 'Cos':
-        lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=3)
-    
-    if args.checkpoint is None:   
-        check_epoch = 0
-        train_acc_list = []
-        valid_acc_list = []
-        train_loss_list = []
-        valid_loss_list = []
-    else:
-        checkpoint = torch.load(args.checkpoint)
-        check_epoch = checkpoint['epoch']
-        model.load_state_dict(checkpoint['model_state_dict'])
-        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-        lr_scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
-        train_acc_list = checkpoint['train_accuracy']
-        valid_acc_list = checkpoint['val_accuracy']
-        train_loss_list = checkpoint['train_loss']
-        valid_loss_list = checkpoint['val_loss']
+    if args.model != 'sum':
+        optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+        if args.scheduler == 'StepLR':
+            lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=LR_STEP, gamma=LR_FACTOR)
+        elif args.scheduler == 'Cos':
+            lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=3)
+        
+        if args.checkpoint is None:   
+            check_epoch = 0
+            train_acc_list = []
+            valid_acc_list = []
+            train_loss_list = []
+            valid_loss_list = []
+        else:
+            checkpoint = torch.load(args.checkpoint)
+            check_epoch = checkpoint['epoch']
+            model.load_state_dict(checkpoint['model_state_dict'])
+            optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+            lr_scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
+            train_acc_list = checkpoint['train_accuracy']
+            valid_acc_list = checkpoint['val_accuracy']
+            train_loss_list = checkpoint['train_loss']
+            valid_loss_list = checkpoint['val_loss']
         
     model.to(device)
 
@@ -191,11 +116,12 @@ if __name__ == '__main__':
     if args.mode == 'train':
         trainset = Dacon(dir=DIR, mode=args.mode, transform=transforms_train)
         if args.cutmix is True:
-            trainset = CutMix(trainset, num_class=NUM_CLASSES, num_mix=2, prob=0.5, beta=1)
+            trainset = CutMix(trainset, num_class=args.NUM_CLASSES, num_mix=2, prob=0.5, beta=1)
         trainloader = DataLoader(trainset, batch_size=args.batchsize, shuffle=True, num_workers=NUM_WORKERS)
         with trange(args.epoch, initial=check_epoch, desc='Loss : 0', leave=True) as pbar:
             for epoch in pbar:
-                train_acc, train_loss = train(trainloader, model, criterion, optimizer)
+                train_acc, train_loss = train(trainloader, model, criterion, optimizer, device, args)
+                pbar.set_description(f"Loss : {train_loss:.3f}")
                 lr_scheduler.step()
                 train_acc_list.append(train_acc/len(trainloader))
                 train_loss_list.append(train_loss.detach().cpu().numpy())
@@ -211,17 +137,18 @@ if __name__ == '__main__':
         num_valid = len(dacon) - num_train
         trainset, validset = random_split(dacon, [num_train, num_valid])
         if args.cutmix is True:
-            trainset = CutMix(trainset, num_class=NUM_CLASSES, num_mix=2, prob=0.5, beta=1)
+            trainset = CutMix(trainset, num_class=args.NUM_CLASSES, num_mix=2, prob=0.5, beta=1)
         trainloader = DataLoader(trainset, batch_size=args.batchsize, shuffle=True, num_workers=NUM_WORKERS)
         validloader = DataLoader(validset, batch_size=args.batchsize, shuffle=True, num_workers=NUM_WORKERS)
         with trange(args.epoch, initial=check_epoch, desc='Loss : 0', leave=True) as pbar:
             for epoch in pbar:
-                train_acc, train_loss = train(trainloader, model, criterion, optimizer)
+                train_acc, train_loss = train(trainloader, model, criterion, optimizer, device, args)
+                pbar.set_description(f"Loss : {train_loss:.3f}")
                 lr_scheduler.step()
                 train_acc_list.append(train_acc/len(trainloader))
                 train_loss_list.append(train_loss.detach().cpu().numpy())
 
-                valid_acc, valid_loss = validation(validloader, model, criterion)
+                valid_acc, valid_loss = validation(validloader, model, criterion, device)
                 valid_acc_list.append(valid_acc/len(validloader))
                 valid_loss_list.append(valid_loss.detach().cpu().numpy())
                 if valid_loss_list[-1].item() == min(valid_loss_list).item():
@@ -234,4 +161,4 @@ if __name__ == '__main__':
     elif args.mode == 'test':
         testset = Dacon(dir=DIR, mode=args.mode, transform=transforms_train)
         testloader = DataLoader(testset, batch_size=args.batchsize, shuffle=False, num_workers=NUM_WORKERS)
-        submission(testloader, model)
+        submission(testloader, model, device)
--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
+import os
 import torch
+import torch.nn as nn
 import matplotlib.pyplot as plt
 import multiprocessing
+import timm
+from efficientnet_pytorch import EfficientNet
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
@@ -52,15 +56,12 @@ def visualize(checkpoint_dir, args):
         ax[0].set_xlabel("Epoch")
         ax[0].plot(range(1, len(train_acc_list)+1), train_acc_list)
         ax[0].legend(['Train'])
-        #ax[0].set_xlim(left=15)
 
         ax[1].set_title("Training Loss")
         ax[1].set_ylabel("Loss")
         ax[1].set_xlabel("Epoch")
         ax[1].plot(range(1, len(train_loss_list)+1), train_loss_list)
         ax[1].legend(['Train'])
-        #ax[1].set_xlim(left=15)
-        #ax[1].set_ylim(top=1.5)
         print(f"LR : {learning_rate}, Batch Size : {batch_size}, 현재 Epoch : {checkpoint['epoch']}")
         print(f"Train 정확도 : {train_acc_list[-1]}, Train Loss : {train_loss_list[-1]}")
         plt.savefig(f'{args.model}_{args.scheduler}_train_fig.png')
@@ -73,7 +74,6 @@ def visualize(checkpoint_dir, args):
         ax[0].plot(range(1, len(train_acc_list)+1), train_acc_list)
         ax[0].plot(range(1, len(val_acc_list)+1), val_acc_list)
         ax[0].legend(['Train', 'Valid'])
-        #ax[0].set_xlim(left=15)
 
         ax[1].set_title("Training/Valid Loss")
         ax[1].set_ylabel("Loss")
@@ -81,8 +81,6 @@ def visualize(checkpoint_dir, args):
         ax[1].plot(range(1, len(train_loss_list)+1), train_loss_list)
         ax[1].plot(range(1, len(val_loss_list)+1), val_loss_list)
         ax[1].legend(['Train', 'Valid'])
-        #ax[1].set_xlim(left=15)
-        #ax[1].set_ylim(top=1.5)
         
         print(f"LR : {learning_rate}, Batch Size : {batch_size}, 현재 Epoch : {checkpoint['epoch']}")
         print(f"Train 정확도 : {train_acc_list[-1]}, Train Loss : {train_loss_list[-1]}")
@@ -91,9 +89,6 @@ def visualize(checkpoint_dir, args):
         plt.savefig(f'{args.model}_{args.scheduler}_val_fig.png')
 
 def get_parameters(dataset, args):
-    '''
-    It takes about 5 minutes
-    '''
     PARAMETERS = {
     'mean':[0, 0, 0],
     'std': [0, 0, 0]
@@ -122,4 +117,117 @@ def get_parameters(dataset, args):
 def get_learing_rate(optimizer):
     for param_group in optimizer.param_groups:
         lr = param_group['lr']
-    return lr
+        return lr
+
+def train(train_loader, model, criterion, optimizer, device, args):
+    model.train()
+    acc = 0
+    for images, labels in train_loader:
+        optimizer.zero_grad()
+        output = model(images.to(device))
+        loss = criterion(output, labels.to(device))
+        loss = (loss-args.flooding).abs()+args.flooding
+        loss.backward()
+        optimizer.step()
+        if args.cutmix is True:
+            acc += (output.softmax(dim=1)*labels.to(device)).sum(dim=1).float().mean().detach().cpu()
+        else:
+            acc += (output.argmax(1)==labels.to(device)).float().mean()
+    return acc, loss
+
+def validation(valid_loader, model, criterion, device):
+    model.eval()
+    acc = 0
+    for images, labels in valid_loader:
+        with torch.no_grad():
+            output = model(images.to(device))
+            loss = criterion(output, labels.to(device))
+            acc += (output.argmax(1)==labels.to(device)).float().mean()
+    return acc, loss
+
+def submission(test_loader, model, device):
+    model.eval()
+    landmark_id = []
+    conf = []
+    for images in tqdm(test_loader):
+        with torch.no_grad():
+            pred = model(images.to(device))
+            pred = nn.Softmax(dim=1)(pred)
+            pred = pred.detach().cpu()
+            landmark_id.append(torch.argmax(pred, dim=1))
+            conf.append(torch.max(pred, dim=1)[0])
+    submission = test_loader.dataset.submission
+    submission.landmark_id = torch.cat(landmark_id).numpy()
+    submission.conf = torch.cat(conf).numpy()
+    submission.to_csv(os.path.join(os.getcwd(), 'submission.csv'), index=False)
+
+class Ensemble_weighted_sum(nn.Module):
+    '''
+    Weighted Sum per Class
+    '''
+    def __init__(self, model_list, num_classes):
+        super(Ensemble_weighted_sum, self).__init__()
+        for idx, model in enumerate(model_list):
+            setattr(self, f'model_{idx}', model)
+            for param in getattr(self, f'model_{idx}').parameters():
+                param.requires_grad=False
+        self.numofmodel = len(model_list)
+        #Create new classifier
+        self.weight = nn.Parameter(torch.randn((len(model_list), num_classes), requires_grad=True)) 
+
+    def forward(self, x):
+        output = []
+        for idx in range(self.numofmodel):
+            output.append(getattr(self, f'model_{idx}')(x))
+
+        output = torch.stack(output, dim=1)
+        output = output * nn.Softmax(dim=0)(self.weight)
+        output = torch.sum(output, dim=1)
+        return output
+
+class Ensemble_sum(nn.Module):
+    '''
+    Sum of Output
+    '''
+    def __init__(self, model_list, num_classes):
+        super(Ensemble_sum, self).__init__()
+        for idx, model in enumerate(model_list):
+            setattr(self, f'model_{idx}', model)
+            for param in getattr(self, f'model_{idx}').parameters():
+                param.requires_grad=False
+        self.numofmodel = len(model_list)
+
+    def forward(self, x):
+        output = []
+        for idx in range(self.numofmodel):
+            output.append(getattr(self, f'model_{idx}')(x))
+        return sum(output)
+
+def load_model(args):
+    #Ensemble model
+    if args.model in ['sum', 'weighted_sum']:
+        path0 = os.getcwd()+'/resnext.pt'
+        path1 = os.getcwd()+'/b1_cos.pt'
+        path2 = os.getcwd()+'/b5.pt'
+        path3 = os.getcwd()+'/vit.pt'
+        model_0 = timm.create_model('skresnext50_32x4d', pretrained=False, num_classes=args.NUM_CLASSES)
+        model_0.load_state_dict(torch.load(path0)['model_state_dict'])
+        model_1 = timm.create_model("efficientnet_b1", pretrained=False, num_classes=args.NUM_CLASSES)
+        model_1.load_state_dict(torch.load(path1)['model_state_dict'])
+        model_2 = timm.create_model("efficientnet_b5", pretrained=False, num_classes=args.NUM_CLASSES)
+        model_2.load_state_dict(torch.load(path2)['model_state_dict'])
+        model_3 = timm.create_model('vit_base_patch16_224', pretrained=False, num_classes=args.NUM_CLASSES)
+        model_3.load_state_dict(torch.load(path3)['model_state_dict'])     
+        model = globals()[f'Ensemble_{args.model}']([model_0, model_1, model_2, model_3], num_classes=args.NUM_CLASSES) 
+    #One model
+    elif args.model == 'vit_base':
+        model = timm.create_model('vit_base_patch16_224', pretrained=True, num_classes=args.NUM_CLASSES)
+    elif args.model == 'vit_base_hybrid':
+        model = timm.create_model('vit_base_resnet26d_224', pretrained=True, num_classes=args.NUM_CLASSES)
+    elif args.model == 'vit_large':
+        model = timm.create_model('vit_large_patch16_224', pretrained=True, num_classes=args.NUM_CLASSES)
+    elif args.model == 'skresnext50':
+        model = timm.create_model('skresnext50_32x4d', pretrained=True, num_classes=args.NUM_CLASSES)
+    else:
+        model = timm.create_model(f"efficientnet_{args.model}", pretrained=True, num_classes=args.NUM_CLASSES)        
+    return model
