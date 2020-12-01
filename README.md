8b9919dd0266d2c31cb734e8f3b01f1cf3a3f192
creamnuts <hong4497@gmail.com>
2020-12-01 23:25:51 +0900

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# dacon_landmark
-Dacon Landmark
+# Dacon: [랜드마크 분류 AI 경진대회](https://dacon.io/competitions/official/235585/overview/)
+## 진행 과정
+* 기존 데이터셋은 사진들과 json이 들어있는데, 이를 이용해 다음과 같이 각 라벨별로 새로 배치
+    ```shell
+    dataset/
+        class0/
+            0.jpg
+            1.jpg
+            ...
+        class1/
+            0.jpg
+            1.jpg
+            ...
+        ...
+    ```
+* 전체 Trainset의 80%를 이용한 학습에서 최고성능을 보인 Epoch을 기록하고, 이후 전체 Trainset을 이용 그 Epoch까지 학습시켜 단일 모델들을 얻음
+* Lr scheduler로 StepLR과 CosAnnealing을 사용해 봤는데, 후자가 성능이 더 좋았음
+* 모든 경우에서 Cutmix가 성능이 제일 좋음
+* Eff_[b0, b1, b5], vit_[base, base_hybrid, large], skresnext50의 모델들을 학습시켜 val loss와 acc를 비교 
+    * vit계열은 base를 제외하고 성능이 다 좋지 않았음
+    * val loss: b5 > skresnext > b0 > b1 > vit
+    * val acc: skresnext > b1 > b5 > b0 > vit
+* 앙상블은 단순히 더한것(sum)과 클래스별로 가중치를 주어 더한것(weighted_sum)을 이용. Public Score가 전자가 더 높아 전자를 제출
+    * sum: 위에서 비교한 5개의 모델을 모두 이용한 것이, b0를 빼고 나머지 4개를 이용한 것 보다 좋지 않아 최종적으로 b1, b5, vit, skresnext를 사용
+    * weighted_sum: 사용한 모델은 sum과 같고, 각 클래스별로 가중치를 주므로 4*1049의 learnable parameter가 추가됨
+* 나름 열심히 한 첫 대회. Public 11등 Private 14등
+
+## 사용 방법
+```shell
+$ python3 main.py -h
+```
+
+```console
+usage: main.py [-h] --mode {train,val,test} --model {sum,weighted_sum,b0,b1,b2,b3,b4,b5,b6,b7,b8,l2,vit_base,vit_base_hybrid,vit_large,skresnext50} [--calculator False] [--checkpoint None] [--save Checkpoint.pt] [--gpu 0]
+               [--cutmix True] [--scheduler {StepLR,Cos}] [--batchsize 128] [--lr 0.001] [--epoch 50] [--flooding 0.01] [--num_classes 1049]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --mode {train,val,test}, -m {train,val,test}
+                        train: Use total dataset, val: Hold-out of 0.8 ratio, test: Make submission
+  --model {sum,weighted_sum,b0,b1,b2,b3,b4,b5,b6,b7,b8,l2,vit_base,vit_base_hybrid,vit_large,skresnext50}
+                        sum and weighted_sum: ensemble, others: efficientnet, vit, skresnext
+  --calculator False    Caculate mean and std of dataset
+  --checkpoint None     Checkpoint directory
+  --save Checkpoint.pt  Save directory. If checkpoint exists, save checkpoint in checkpoint dir
+  --gpu 0               GPU number to use
+  --cutmix True         If True, use Cutmix aug in training
+  --scheduler {StepLR,Cos}
+                        Cos: cosine annealing
+  --batchsize 128
+  --lr 0.001
+  --epoch 50
+  --flooding 0.01
+  --num_classes 1049    Number of classes
+```
--- main.py
+++ main.py
@@ -20,25 +20,25 @@ from cutmix.utils import CutMixCrossEntropyLoss
 
 #########setting hyperparameters in here########
 parser = argparse.ArgumentParser()
-parser.add_argument('--mode', '-m', default='val', choices=['train', 'val', 'test'], help='Train : Use Total Dataset, Val : Use Hold-Out, Test : Make Submission')
-parser.add_argument('--calculator', default='False', help='Cacluate Dataset Mean and Std')
-parser.add_argument('--checkpoint', '-c', default=None, help='Checkpoint Directory')
-parser.add_argument('--save', '-s', default='./Checkpoint.pt', help='Save Directory. if Checkpoint exists, Save Checkpoint in Checkpoint Dir')
-parser.add_argument('--model', default='b1', choices=['sum', 'weighted_sum', 'b0', 'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'l2', 'vit_base', 'vit_base_hybrid', 'vit_large', 'skresnext50'])
-parser.add_argument('--gpu', default='0')
-parser.add_argument('--cutmix', default=True, help="If True, Use Cutmix Aug in Training")
-parser.add_argument('--scheduler', default='Cos', choices=['StepLR', 'Cos'])
-parser.add_argument('--batchsize', type=int, default=128)
-parser.add_argument('--lr', type=float, default=1e-3)
-parser.add_argument('--epoch', type=int, default=50)
-parser.add_argument('--flooding', type=float, default=0.01)
-parser.add_argument('--num_classes', dest='NUM_CLASSES', type=int, default=1049)
+parser.add_argument('--mode', '-m', required=True, choices=['train', 'val', 'test'], help='train: Use total dataset, val: Hold-out of 0.8 ratio, test: Make submission')
+parser.add_argument('--model', required=True, choices=['sum', 'weighted_sum', 'b0', 'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'l2', 'vit_base', 'vit_base_hybrid', 'vit_large', 'skresnext50'], help='sum and weighted_sum: ensemble, others: efficientnet, vit, skresnext')
+parser.add_argument('--calculator', metavar=False, default=False, help='Caculate mean and std of dataset')
+parser.add_argument('--checkpoint', metavar='None', default=None, help='Checkpoint directory')
+parser.add_argument('--save', metavar='Checkpoint.pt', default='./Checkpoint.pt', help='Save directory. If checkpoint exists, save checkpoint in checkpoint dir')
+parser.add_argument('--gpu', metavar=0, default='0', help='GPU number to use')
+parser.add_argument('--cutmix', default=True, metavar='True', help="If True, use Cutmix aug in training")
+parser.add_argument('--scheduler', default='Cos', choices=['StepLR', 'Cos'], help='Cos: cosine annealing')
+parser.add_argument('--batchsize', type=int, metavar=128, default=128)
+parser.add_argument('--lr', type=float, metavar=1e-3, default=1e-3)
+parser.add_argument('--epoch', type=int, metavar=50, default=50)
+parser.add_argument('--flooding', type=float, metavar=0.01, default=0.01)
+parser.add_argument('--num_classes', dest='NUM_CLASSES', metavar=1049, type=int, default=1049, help='Number of classes')
 args = parser.parse_args()
 
 DIR = os.path.join(os.getcwd(),'..', 'data') + '/'
 LR_STEP = 3
 LR_FACTOR = 0.5
-NUM_WORKERS = 4 #multiprocessing.cpu_count() #24
+NUM_WORKERS = 4
 
 torch.manual_seed(777)
 torch.cuda.manual_seed(777)
@@ -51,12 +51,12 @@ os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print('use: ',device)
 
+# Pre-calculated value
 TRAIN_PARAMETERS = {
     'mean':[0.4461, 0.4447, 0.4490],
     'std':[0.2598, 0.2591, 0.2590]
 }
 
-################################################
 calculation = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor()
@@ -66,7 +66,6 @@ if __name__ == '__main__':
     if args.calculator is True:
         cal_dataset = Dacon(dir=DIR, mode=args.mode, transform=calculation)
         PARAMETERS = get_parameters(cal_dataset, args)
-    
         transforms_train = transforms.Compose([
             transforms.Resize((224, 224)),
             transforms.ToTensor(),
@@ -82,7 +81,7 @@ if __name__ == '__main__':
     
     model = load_model(args)
     
-    if args.model != 'sum':
+    if args.model != 'sum': # Sum ensemble model doesn't have trainable parameters
         optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
         if args.scheduler == 'StepLR':
             lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=LR_STEP, gamma=LR_FACTOR)
@@ -91,10 +90,7 @@ if __name__ == '__main__':
         
         if args.checkpoint is None:   
             check_epoch = 0
-            train_acc_list = []
-            valid_acc_list = []
-            train_loss_list = []
-            valid_loss_list = []
+            train_acc_list, valid_acc_list, train_loss_list, valid_loss_list = [], [], [], []
         else:
             checkpoint = torch.load(args.checkpoint)
             check_epoch = checkpoint['epoch']
